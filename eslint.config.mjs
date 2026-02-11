95552ec699204d0069ccae1adfb1affcdd939cbe
sangeric <eric.sang.pro@gmail.com>
2026-02-11 10:10:48 +0100

    Ajout de github actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,31 @@
 import js from "@eslint/js";
+import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "commonjs"
-    },
-    rules: {
-      "indent": ["error", 2],
-      "quotes": ["error", "single"],
-      "semi": ["error", "always"],
-      "no-console": "warn",
-      "no-unused-vars": "error",
-      "eqeqeq": ["error", "always"],
-      "curly": ["error", "all"],
-      "space-before-function-paren": ["error", "never"],
-      "comma-dangle": ["error", "never"],
-      "object-curly-spacing": ["error", "always"],
-      "array-bracket-spacing": ["error", "never"]
-    }
+    {ignores : ["eslint.config.mjs"]},
+{
+  files: ["**/*.{js,mjs,cjs}"],
+  plugins: { js },
+  languageOptions: {
+  globals: {...globals.node,...globals.browser},
+  ecmaVersion: "latest",
+  sourceType: "commonjs"
+},
+
+rules: {
+  "indent": ["error", 2],
+  "quotes": ["error", "single"],
+  "semi": ["error", "always"],
+  "no-console": "off",
+  "no-unused-vars": "error",
+  "eqeqeq": ["error", "always"],
+  "curly": ["error", "all"],
+  "space-before-function-paren": ["error", "never"],
+  "comma-dangle": ["error", "never"],
+  "object-curly-spacing": ["error", "always"],
+  "array-bracket-spacing": ["error", "never"]
   }
+},
+js.configs.recommended
 ]);
